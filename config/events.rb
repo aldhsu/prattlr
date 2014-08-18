115272d4523f899e428637ab0eb85cf8b45a5c13
Allen <0allen.hsu@gmail.com>
2014-08-18 20:40:37 +1000

    setup basic chat

--- config/events.rb
+++ config/events.rb
@@ -12,3 +12,11 @@ WebsocketRails::EventMap.describe do
   #   end
   # The above will handle an event triggered on the client like `product.new`.
 end
+
+WebsocketRails::EventMap.describe do
+  subscribe :client_connected, to: ChatController, with_method: :client_connected
+  subscribe :new_message, to: ChatController, with_method: :new_message
+  subscribe :new_user, to: ChatController, with_method: :new_user
+  subscribe :change_username, to: ChatController, with_method: :change_username
+  subscribe :client_disconnected, to: ChatController, with_method: :delete_user
+end
